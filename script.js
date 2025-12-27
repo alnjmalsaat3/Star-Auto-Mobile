e1374aa154e0ed63fe2b283f2f28e3d9adc1cbf1
alnjmalsaat3 <83434407+alnjmalsaat3@users.noreply.github.com>
2025-12-27 12:07:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -59,13 +59,13 @@ fetch('data.json')
         }
 
         // تحميل vCard
-        window.downloadVCard = function() {
+            window.downloadVCard = function() {
             const vCardData = [
                 'BEGIN:VCARD',
                 'VERSION:3.0',
-                ORG:${elements.company},
-                N:${(elements.name || '').split(' ').reverse().join(';')},
-                FN:${elements.name},
+                `ORG:${elements.company}`,
+                `N:${(elements.name || '').split(' ').reverse().join(';')}`,
+                `FN:${elements.name}`,
                 `TITLE:${elements.title}`,
                 elements.email ? `EMAIL;TYPE=PREF:${elements.email}` : '',
                 elements.email1 ? `EMAIL;TYPE=WORK:${elements.email1}` : '',
@@ -109,6 +109,7 @@ fetch('data.json')
         console.error('خطأ في تحميل البيانات:', error);
         alert('حدث خطأ في تحميل البيانات، يرجى التحقق من اتصال الإنترنت أو ملف data.json');
     });
+
 
 
 
